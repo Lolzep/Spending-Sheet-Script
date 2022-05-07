d903c2509c507cb97b4e644dc80245d53aa51c1a
Lolzep <kjkinzel@gmail.com>
2022-05-07 00:18:45 -0500

    Cleaned up weeklyupdate, added comments

--- Spending Sheet Script.js
+++ Spending Sheet Script.js
@@ -1,11 +1,12 @@
 /** @OnlyCurrentDoc */
-/* Transposes an array (col -> rows, rows -> col). It's magic.*/
 function transpose(a)
+/* Transposes an array (col -> rows, rows -> col). It's magic.*/
 {
   return Object.keys(a[0]).map(function (c) { return a.map(function (r) { return r[c]; }); });
 }
 
 function getSheetWithRange(sheettoread, sheettowrite, sourcerange, endrange) {
+  
   /* Get the spreadsheet we need to work on */
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   /* sheetsource = where the values come from, sheet = where the values go */
@@ -31,23 +32,22 @@ function gasupdate() {
   var gastrackerrange = sheetsource.getRange("D56:D59");
   var gastrackervalues = gastrackerrange.getValues();
 
+  /* Get variables from the Gas Tracker range and the Daily Inputs range*/
   const lastpurchasedate = gastrackervalues[0][0];
   const amountpaid = sourcevalues[1][0];
   const today = new Date();
 
+  /* Calculate the date difference since last gas purchase to then calculate dollars/day */
   var datedif = Math.abs(today - lastpurchasedate)
   var datedif =  Math.floor(datedif / (1000 * 60 * 60 * 24));
   const dollarperday = amountpaid / datedif;
-  
-  console.log(lastpurchasedate);
-  console.log(amountpaid);
-  console.log(today);
-  console.log(dollarperday);
 
+  /* Create an array of arrays (rows) to update Gas Tracker section with */
   gastracker = [
     [today], [amountpaid], [today], [dollarperday]
   ];
 
+  /* Create a new array of rows [date, dollars/day] for every day of the week since last purchase */
   var start = lastpurchasedate;
   var end = today;
 
@@ -59,12 +59,11 @@ function gasupdate() {
     loop = new Date(newDate);
   }  
 
-  console.log(weeklydates);
-  console.log(weeklydates.length);
-
+  /* Append new rows just created into the Gas sheet as the new array created */
   LockService.getScriptLock().waitLock(60000);
     sheet.getRange(sheet.getLastRow() + 1, 1, weeklydates.length, weeklydates[0].length).setValues(weeklydates);
 
+  /* Write the values to the Gas Tracker and reset the timer to start at TODAY() */
   gastrackerrange.setValues(gastracker);
   sheetsource.getRange("D58").setFormula("=TODAY()");
 }
@@ -124,21 +123,20 @@ function recordHistory() {
   /* Set the correct date format for the date column */
   const endrange = sheet.getRange("A2:A");
   endrange.setNumberFormat("MM/dd/yyyy");
-
-  console.log(toappend);
 }
 
-function datesAsRow(daysinpast, daysinfuture) {
+function datesAsRows(daysinpast, daysinfuture) {
+
   /* Creates two new strings (start date, end date) */
   var start = new Date();
-  var start = new Date(start.setDate(start.getDate() - daysinpast));
+  var start = new Date(start.setDate(start.getDate() - parseInt(daysinpast)));
   var enddate= new Date();
-  var enddate = new Date(enddate.setDate(enddate.getDate() + daysinfuture));
+  var enddate = new Date(enddate.setDate(enddate.getDate() + parseInt(daysinfuture)));
   var replacedate = Utilities.formatDate(enddate, "GMT-4", "MM/dd/yyyy");
   var end = new Date(replacedate);
 
   /* Loop starting from start of the week and ending at end of week */
-  /* Create a new array for the n days to write in a range*/
+  /* Create a new array of rows for n days*/
   var weeklydates = [[start]];
   var loop = new Date(start);
   while (loop <= end) {
@@ -153,12 +151,18 @@ function datesAsRow(daysinpast, daysinfuture) {
 function weeklyupdate() {
 
   /* Use getSheetWithRange to find sheets to read and write to */
-  var {source} = getSheetWithRange("Job", "History", "I5:O5");
+  var {sheetsource, source} = getSheetWithRange("Job", "History", "I5:O5");
 
-  var weeklydates = datesAsRow(0, 6);
+  /* Use the datesAsRows function to write this week and last weeks dates into as an array */
+  var thisweek = datesAsRows(7, -1);
+  var lastweek = datesAsRows(0, 6);
 
   /* Transpose the array so that columns are inserted instead of rows */
-  /* Write the values into the source sheet */
-  var tweeklydates = transpose(weeklydates);
-  source.setValues(tweeklydates);
+  /* Write the values into the source sheet for this week and last week */
+  var hthisweek = transpose(thisweek);
+  source.setValues(hthisweek);
+
+  var source = sheetsource.getRange("I22:O22");
+  var hlastweek = transpose(lastweek);
+  source.setValues(hlastweek);
 }
